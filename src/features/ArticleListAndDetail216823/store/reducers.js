0f39ca3babbc27d0ab374d5d2d8e20da1c838bfa
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-05 21:11:28 +0000

    Remove mock data

--- src/features/ArticleListAndDetail216823/store/reducers.js
+++ src/features/ArticleListAndDetail216823/store/reducers.js
@@ -4,26 +4,7 @@ const initialState = {
   api: {
     isFetching: false,
   },
-  articles: [
-    {
-      "id": 1,
-      "title": "Article #1",
-      "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "author": 1,
-      "image": null,
-      "created_at": "2021-01-05T16:42:44.079109Z",
-      "updated_at": "2021-01-05T16:42:44.079156Z"
-    },
-    {
-      "id": 2,
-      "title": "Article #2",
-      "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "author": 1,
-      "image": "https://mobile-0104-dev-17890.s3.amazonaws.com/media/articles/Hyhyr9c51n5cYNpx.png",
-      "created_at": "2021-01-05T16:44:09.312976Z",
-      "updated_at": "2021-01-05T16:44:09.313037Z"
-    }
-  ]
+  articles: []
 }
 
 export default function reducer(state = initialState, action) {
