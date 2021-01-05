460f6d89fae31f6a2768fa55acc533ad0f6179db
Crowdbotics <team@crowdbotics.com>
2021-01-05 17:41:14 +0000

    Update models

--- backend/tst_daniel_scaffold_17959/wsgi.py
+++ backend/tst_daniel_scaffold_17959/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_daniel_scaffold_17959.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_daniel_scaffold_17959.settings")
 
 application = get_wsgi_application()
